001d9aa73b3f5aef5e5aac37b2b2317858a5ab00
Pkoka99 <zekazeus123@gmail.com>
2025-07-06 03:10:15 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,13 +1,11 @@
-apt update && apt upgrade -y
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-apt --fix-broken install -y
-dpkg -i google-chrome-stable_current_amd64.deb
-wget https://storage.googleapis.com/chrome-for-testing-public/137.0.7151.103/linux64/chromedriver-linux64.zip
-unzip chromedriver-linux64.zip
-chmod +x chromedriver-linux64/chromedriver
-mv chromedriver-linux64/chromedriver /usr/local/bin/
-apt install python3-pip -y
-python3 -m pip install selenium
-wget https://raw.githubusercontent.com/Pkoka99/Pkoka99/refs/heads/main/vv.py
-python3 vv.py
+wget https://raw.githubusercontent.com/Pkoka99/poka1/refs/heads/main/m.py
+rm -r tmate.txt
+pkill -9 tmate
+wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-static-linux-i386.tar.xz &>/dev/null
+tar --skip-old-files -xf tmate-2.4.0-static-linux-i386.tar.xz &>/dev/null
+bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
+# simpan link ke file
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "#{tmate_ssh} -t" > tmate.txt
+echo "Link tmate tersimpan di tmate.txt"
+python m.py
